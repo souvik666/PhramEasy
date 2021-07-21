148b64515a835ac22447b3e8800271704d8e5f3f
souvik666 <peacedynasty0@gmail.com>
2021-07-21 11:31:46 +0530

    timer with slidebar add css

--- timer with slidebar add css/timer.js
+++ timer with slidebar add css/timer.js
@@ -1,3 +1,4 @@
+//product data set add products here
 let data = [
   {
     name: "Sensodyne",
@@ -85,6 +86,7 @@ let data = [
 if (localStorage.getItem("pro") == null) {
   localStorage.setItem("pro", JSON.stringify(data));
 }
+
 // making html structure for cards
 function appendpro() {
   let data = JSON.parse(localStorage.getItem("pro"));
@@ -131,7 +133,9 @@ function appendpro() {
     });
   });
 }
-appendpro();
+
+appendpro(); //calling appendpro function
+
 //add to cart method
 function addToCart(obj) {
   let cart = [];
@@ -144,3 +148,79 @@ function addToCart(obj) {
   cart.push(obj);
   localStorage.setItem("cart", JSON.stringify(cart));
 }
+
+// onclikc event for slider
+function slider() {
+  let sapn = document.getElementsByTagName("span");
+  let div = document.getElementsByTagName("div");
+  let l = 0;
+  sapn[1].onclick = () => {
+    l++; //for right scroll
+    for (var i of div) {
+      if (l == 0) {
+        i.style.left = "0px";
+      }
+      if (l == 1) {
+        i.style.left = "-740px";
+      }
+      if (l == 2) {
+        i.style.left = "-1480px";
+      }
+      if (l == 3) {
+        i.style.left = "-2220px";
+      }
+      if (l == 4) {
+        i.style.left = "-2967px";
+      }
+      if (l > 4) {
+        l = 4;
+      }
+    }
+  };
+  sapn[0].onclick = () => {
+    l--; //for left scroll
+    for (var i of div) {
+      if (l == 0) {
+        i.style.left = "0px";
+      }
+      if (l == 1) {
+        i.style.left = "-740px";
+      }
+      if (l == 2) {
+        i.style.left = "-1480px";
+      }
+      if (l == 3) {
+        i.style.left = "-2220px";
+      }
+
+      if (l < 0) {
+        l = 0;
+      }
+    }
+  };
+}
+//count down timer
+var countDownDate = new Date("Jul 29, 2021 15:37:25").getTime();
+var x = setInterval(function () {
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="box"
+  document.getElementById("box").innerHTML =
+    days + "d " + hours + "h " + minutes + "m " + seconds + "s " + "left";
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("box").innerHTML = "EXPIRED";
+  }
+}, 1000);
